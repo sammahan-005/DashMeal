1b4429c8c44466a984d94d89c6d9406ce07e53b7
nitoueksamy@gmail.com <nitoueksamy@gmail.com>
2026-04-17 15:16:25 +0100

    Deploy

--- start/env.ts
+++ start/env.ts
@@ -16,8 +16,12 @@ export default await Env.create(new URL('../', import.meta.url), {
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
   PORT: Env.schema.number(),
   HOST: Env.schema.string({ format: 'host' }),
-  LOG_LEVEL: Env.schema.string(),
-  DB_PORT: Env.schema.number(),
+  LOG_LEVEL: Env.schema.string.optional(),
+  DB_HOST: Env.schema.string.optional(),
+  DB_PORT: Env.schema.number.optional(),
+  DB_USER: Env.schema.string.optional(),
+  DB_PASSWORD: Env.schema.string.optional(),
+  DB_DATABASE: Env.schema.string.optional(),
 
   // App
   APP_KEY: Env.schema.secret(),
